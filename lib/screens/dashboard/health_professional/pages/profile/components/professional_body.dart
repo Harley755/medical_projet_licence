68f1330ba480c076de93290fc8c40902e49ff8a3
Brice G <bricegoudalo@gmail.com>
2023-05-02 14:21:18 +0200

    Add Sub menu to professional

--- lib/screens/dashboard/health_professional/pages/profile/components/professional_body.dart
+++ lib/screens/dashboard/health_professional/pages/profile/components/professional_body.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:medical_projet/screens/dashboard/health_professional/pages/profile/components/professional_profile_menu.dart';
 import 'package:medical_projet/screens/dashboard/health_professional/pages/profile/components/professional_profile_pic.dart';
+import 'package:medical_projet/screens/dashboard/health_professional/pages/profile/components/professional_sub_profile_menu.dart';
 import 'package:medical_projet/screens/dashboard/health_professional/pages/profile/identity/professional_profile_identity.dart';
 
-import 'professional_profile_menu.dart';
-
-class ProfessionalBody extends StatelessWidget {
+class ProfessionalBody extends StatefulWidget {
   const ProfessionalBody({super.key});
 
+  @override
+  State<ProfessionalBody> createState() => _ProfessionalBodyState();
+}
+
+class _ProfessionalBodyState extends State<ProfessionalBody> {
+  bool isClicked = false;
+  bool isClickedToogle = false;
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -15,24 +23,73 @@ class ProfessionalBody extends StatelessWidget {
         children: [
           const ProfessionalProfilePic(),
           const SizedBox(height: 20),
+          // MES INFORMATIONS D'IDENTITE
           ProfessionalProfileMenu(
             text: "Mes informations d'identité",
             icon: "assets/icons/User Icon.svg",
-            press: () => {
-              Navigator.pushNamed(
-                  context, ProfessionalProfileIdentity.routeName),
+            press: () => Navigator.pushNamed(
+              context,
+              ProfessionalProfileIdentity.routeName,
+            ),
+          ),
+
+          // SECURITE DROPDOWN
+          ProfessionalProfileMenu(
+            bottomPadding: 5,
+            clickToogle: isClickedToogle,
+            text: "Sécurité",
+            icon: "assets/icons/Lock.svg",
+            press: () {
+              setState(() {
+                isClicked = !isClicked;
+                isClickedToogle = !isClickedToogle;
+              });
             },
           ),
+          // DROPDOWN CHILD
+          isClicked
+              ? Column(
+                  children: [
+                    ProfessionalSubProfileMenu(
+                      text: "Changer d'adresse email",
+                      icon: "assets/icons/Lock.svg",
+                      press: () => {},
+                    ),
+                    ProfessionalSubProfileMenu(
+                      text: "Ajouter des empreintes",
+                      icon: "assets/icons/Lock.svg",
+                      press: () => {},
+                    ),
+                    ProfessionalSubProfileMenu(
+                      text: "Changer de mot de passe",
+                      icon: "assets/icons/Lock.svg",
+                      press: () => {},
+                    ),
+                    ProfessionalSubProfileMenu(
+                      downPadding: 14,
+                      text: "Changer de numéro de téléphone",
+                      icon: "assets/icons/Lock.svg",
+                      press: () => {},
+                    ),
+                  ],
+                )
+              : Container(),
+
+          // TOOGLE DARK/WHITE MODE AND CHANGE LANGUAGE
           ProfessionalProfileMenu(
             text: "Parametre",
             icon: "assets/icons/Settings.svg",
             press: () {},
           ),
+
+          /// WRITE TO CENTER FOR FEEDBACK
           ProfessionalProfileMenu(
             text: "Demande d'aide",
             icon: "assets/icons/Question mark.svg",
             press: () {},
           ),
+
+          // LOG OUT
           ProfessionalProfileMenu(
             text: "Déconnexion",
             icon: "assets/icons/Log out.svg",
