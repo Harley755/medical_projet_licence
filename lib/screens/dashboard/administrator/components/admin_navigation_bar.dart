ece85ba342e9323335ae46938775fd165c0e52f3
Brice G <bricegoudalo@gmail.com>
2023-05-03 19:44:59 +0200

    rm unless package

--- lib/screens/dashboard/administrator/components/admin_navigation_bar.dart
+++ lib/screens/dashboard/administrator/components/admin_navigation_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:medical_projet/constants.dart';
